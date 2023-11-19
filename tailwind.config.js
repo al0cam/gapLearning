806d54f952d8300ae1599d67c45cfd97ae4e34e8
Al0cam <benjaminsikac@gmail.com>
2023-11-19 20:10:47 +0100

    added color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      
+    },
+  }, 
   daisyui: {
     themes: [
       {
         mytheme: {
-            "primary": "#c65e47",
-            "secondary": "#161bad",
-            "accent": "#2070f9",
-            "neutral": "#2b243d",
-            "base-100": "#382c3a",
-            "info": "#91c2de",
-            "success": "#149049",
-            "warning": "#fab20a",
-            "error": "#f94e4e",
+          "primary": "#db76af",
+          "secondary": "#c8f788",
+          "accent": "#55aac6",
+          "neutral": "#281528",
+          "base-100": "#34353d",
+          "info": "#56a3cd",
+          "success": "#137748",
+          "warning": "#f7bf18",
+          "error": "#ea6276",
           },
       },
     ],
