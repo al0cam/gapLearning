4a02492e3207f5a2ac9cc0e8e9a1553731899360
Al0cam <benjaminsikac@gmail.com>
2023-11-19 20:11:11 +0100

    defined the navbar as a different component and modified app accordingly

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
   import { timerStore } from "./Stores/TimerStore";
+  import Navbar from "./lib/Navbar.svelte";
+  import Settings from "./lib/Settings.svelte";
   import Timer from "./lib/Timer.svelte";
 
   timerStore;
 </script>
 
-<div class="bg-base-300 min-w-screen min-h-screen">
+<div class="bg-base-200 min-w-screen min-h-screen">
   <div
-    class="m-0 mx-auto flex flex-col gap-3 p-3 md:w-5/12 w-full min-h-screen fixed top-0 inset-x-0 bg-rose-200 text-black"
+    class="m-0 mx-auto flex flex-col gap-3 p-3 lg:w-5/12 w-full min-h-screen fixed top-0 inset-x-0 bg-base-100 text-black"
   >
     <header>
-      <h1 class="text-7xl">Gap Learning</h1>
+      <Navbar />
     </header>
     <div class="divider" />
 
     <main class="flex flex-col gap-3 items-center w-full">
       <Timer />
+
+      <div>
+        <Settings />
+      </div>
     </main>
 
     <footer />
