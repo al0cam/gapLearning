887491f33a1234e5f602cff42e615137f2c4d9d0
Al0cam <benjaminsikac@gmail.com>
2023-11-21 21:20:56 +0100

    added settings dialog design and hour, minute and second manipulation

--- src/lib/Settings.svelte
+++ src/lib/Settings.svelte
@@ -1,19 +1,66 @@
 <script lang="ts">
   import { settingsStore } from "../Stores/SettingsStore";
+  import { timerStore } from "../Stores/TimerStore";
   import CancelIcon from "../assets/CancelIcon.svg";
+
+  let modal: HTMLDialogElement;
+
+  $: if (modal instanceof HTMLDialogElement) modal.showModal();
 </script>
 
+<!-- TODO: i dont like the fact that the modal is in the center of the page at the moment
+for some reason i feel like the settings should start from the top and go to the end -->
+
+<!-- dialog base (background) -->
 <dialog
-  bind:this={$settingsStore.modal}
-  class="rounded-lg mx-auto md:w-2/5 w-full gap-5"
+  bind:this={modal}
+  class="rounded-lg mx-auto md:w-2/5 w-full flex flex-col gap-3 p-3"
 >
-  <div class="flex flex-row justify-end">
+  <!-- dialog header together with close button -->
+  <div class="flex flex-row justify-between">
+    <span class="text-3xl font-bold">Settings</span>
     <button class="btn btn-secondary" on:click={settingsStore.closeModal}>
       <img src={CancelIcon} alt="Yes" class="w-full h-full p-2" />
     </button>
   </div>
-  <div>settings</div>
+  <!-- dialog content -->
+  <div class="flex flex-col gap-3">
+    <div>
+      <!-- subtitle -->
+      <span class="text-xl">Time</span>
+      <!-- content of subtitle -->
+      <div class="flex flex-row gap-2 join">
+        <div class="flex flex-col items-center">
+          <span>Hours</span>
+          <input
+            class="w-14 text-center rounded-md"
+            type="number"
+            bind:value={$timerStore.hours}
+          />
+        </div>
+        <div class="flex flex-col items-center">
+          <span>Minutes</span>
+          <input
+            class="w-14 text-center rounded-md"
+            type="number"
+            bind:value={$timerStore.minutes}
+          />
+        </div>
+        <div class="flex flex-col items-center">
+          <span>Seconds</span>
+          <input
+            class="w-14 text-center rounded-md"
+            type="number"
+            bind:value={$timerStore.seconds}
+          />
+        </div>
+      </div>
+      <!-- dialog footer -->
+      <div class="flex flex-row justify-end">
+        <button class="btn btn-primary" on:click={settingsStore.closeModal}>
+          Save
+        </button>
+      </div>
+    </div>
+  </div>
 </dialog>
-
-<!-- TODO: how to execute call from navbar to other component like settings -->
-<!-- maybe through parent but is there an option for cross-non-parent interaction -->
