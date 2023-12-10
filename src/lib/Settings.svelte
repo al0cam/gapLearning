c73947bf077820c0e65afc456e3052794be4b106
Al0cam <benjaminsikac@gmail.com>
2023-12-10 18:58:29 +0100

    fixed always displayed dialog

--- src/lib/Settings.svelte
+++ src/lib/Settings.svelte
@@ -4,17 +4,15 @@
   import { languageStore } from "../Stores/LanguageStore";
   import CancelIcon from "../assets/CancelIcon.svg";
 
-  let modal: HTMLDialogElement;
   let selectedLanguage: string = languageStore.getDefaultLanguage();
 
   function selectLanguage() {
-    console.log("OK");
     languageStore.setLanguage(selectedLanguage);
-    console.log($languageStore["language"]);
-    console.log($languageStore);
   }
 
-  $: if (modal instanceof HTMLDialogElement) modal.showModal();
+  // HACK FOR PERMA DISPLAYING THE MODAL WHILE WORKING ON IT
+  // let modal: HTMLDialogElement;
+  // $: if (modal instanceof HTMLDialogElement) modal.showModal();
 </script>
 
 <!-- TODO: i dont like the fact that the modal is in the center of the page at the moment
@@ -22,72 +20,74 @@ for some reason i feel like the settings should start from the top and go to the
 
 <!-- dialog base (background) -->
 <dialog
-  bind:this={modal}
-  class="rounded-lg mx-auto md:w-2/5 w-full flex flex-col gap-3 p-3"
+  bind:this={$settingsStore.modal}
+  class="rounded-lg mx-auto md:w-2/5 w-full p-3"
 >
-  <!-- HEADER START -->
-  <div class="flex flex-row justify-between">
-    <span class="text-3xl font-bold">{$languageStore["settings"]}</span>
-    <button class="btn btn-secondary" on:click={settingsStore.closeModal}>
-      <img src={CancelIcon} alt="Yes" class="w-full h-full p-2" />
-    </button>
-  </div>
-  <!-- HEADER END -->
   <div class="flex flex-col gap-3">
-    <!-- CONTENT -->
-    <div>
-      <!-- TIMER -->
-      <span class="text-2xl pb-2">{$languageStore["time"]}</span>
-      <div class="flex flex-row gap-3">
-        <div class="flex flex-col items-center">
-          <span>{$languageStore["hours"]}</span>
-          <input
-            class="w-14 text-center rounded-md"
-            type="number"
-            bind:value={$timerStore.hours}
-          />
+    <!-- HEADER START -->
+    <div class="flex flex-row justify-between">
+      <span class="text-3xl font-bold">{$languageStore["settings"]}</span>
+      <button class="btn btn-secondary" on:click={settingsStore.closeModal}>
+        <img src={CancelIcon} alt="Yes" class="w-full h-full p-2" />
+      </button>
+    </div>
+    <!-- HEADER END -->
+    <div class="flex flex-col gap-3">
+      <!-- CONTENT -->
+      <div>
+        <!-- TIMER -->
+        <span class="text-2xl pb-2">{$languageStore["time"]}</span>
+        <div class="flex flex-row gap-3">
+          <div class="flex flex-col items-center">
+            <span>{$languageStore["hours"]}</span>
+            <input
+              class="w-14 text-center rounded-md"
+              type="number"
+              bind:value={$timerStore.hours}
+            />
+          </div>
+          <div class="flex flex-col items-center">
+            <span>{$languageStore["minutes"]}</span>
+            <input
+              class="w-14 text-center rounded-md"
+              type="number"
+              bind:value={$timerStore.minutes}
+            />
+          </div>
+          <div class="flex flex-col items-center">
+            <span>{$languageStore["seconds"]}</span>
+            <input
+              class="w-14 text-center rounded-md"
+              type="number"
+              bind:value={$timerStore.seconds}
+            />
+          </div>
         </div>
-        <div class="flex flex-col items-center">
-          <span>{$languageStore["minutes"]}</span>
-          <input
-            class="w-14 text-center rounded-md"
-            type="number"
-            bind:value={$timerStore.minutes}
-          />
-        </div>
-        <div class="flex flex-col items-center">
-          <span>{$languageStore["seconds"]}</span>
-          <input
-            class="w-14 text-center rounded-md"
-            type="number"
-            bind:value={$timerStore.seconds}
-          />
-        </div>
+        <!-- TIMER END -->
       </div>
-      <!-- TIMER END -->
-    </div>
 
-    <div class="flex flex-col gap-1">
-      <!-- LANGUAGE -->
-      <span class="text-2xl">{$languageStore["language"]}</span>
-      <select
-        class="select w-min max-w-xs"
-        bind:value={selectedLanguage}
-        on:change={() => selectLanguage()}
-      >
-        {#each languageStore.getLanguages() as language}
-          <option value={language}>{language}</option>
-        {/each}
-      </select>
-      <!-- LANGUAGE END -->
+      <div class="flex flex-col gap-1">
+        <!-- LANGUAGE -->
+        <span class="text-2xl">{$languageStore["language"]}</span>
+        <select
+          class="select w-min max-w-xs"
+          bind:value={selectedLanguage}
+          on:change={() => selectLanguage()}
+        >
+          {#each languageStore.getLanguages() as language}
+            <option value={language}>{language}</option>
+          {/each}
+        </select>
+        <!-- LANGUAGE END -->
+      </div>
+      <!-- CONTENT END -->
     </div>
-    <!-- CONTENT END -->
+    <!-- FOOTER -->
+    <!-- <div class="flex flex-row justify-end">
+      <button class="btn btn-primary" on:click={settingsStore.closeModal}>
+        Save
+      </button>
+    </div> -->
+    <!-- FOOTER END -->
   </div>
-  <!-- FOOTER -->
-  <!-- <div class="flex flex-row justify-end">
-    <button class="btn btn-primary" on:click={settingsStore.closeModal}>
-      Save
-    </button>
-  </div> -->
-  <!-- FOOTER END -->
 </dialog>
