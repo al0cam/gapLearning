1ea8b3e400ac9e75b3fa5462037b98b0d8a95005
Al0cam <benjaminsikac@gmail.com>
2023-12-11 22:19:57 +0100

    refactored the store to stop the timer and reset it when it hits 00

--- src/Stores/TimerStore.ts
+++ src/Stores/TimerStore.ts
@@ -1,18 +1,14 @@
-import { derived, writable } from "svelte/store";
+import { writable } from "svelte/store";
 import { Timer } from "../Models/Timer";
 
 function createStore (){
     const { subscribe, set, update } = writable(new Timer(0, 20, 0));
 
     // need method tick that will decrement the timer by 1 second
-    let interval: number | null = null;
+    let interval: number | undefined;
+
     // TODO: add short and long break time
     // TODO: Add set times to cookies so the user doesnt have to add them 10 times over and over
-
-    const tickingHours = derived(
-        timerStore,
-        $timerStore => $timerStore.hours,
-    );
 
     return {
         subscribe,
@@ -28,7 +24,16 @@ function createStore (){
         setHours: (hours: number) => update(timer => timer.setHours(hours)),
         setMinutes: (minutes: number) => update(timer => timer.setMinutes(minutes)),
         setSeconds: (seconds: number) => update(timer => timer.setSeconds(seconds)),
-        start: () => interval = setInterval(() => update(timer => timer.tick()), 1000),
+        start: () => interval = setInterval(() => {
+            update(timer => timer.tick());
+            subscribe(timer => {
+                if(timer.isFinished()){
+                    console.log("Timer is finished");
+                    timerStore.stop();
+                    timerStore.reset();
+                }
+            });
+        } , 1000),
         stop: () => interval ? clearInterval(interval): null
     }
 }
